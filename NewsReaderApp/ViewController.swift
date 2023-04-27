3f94b301520f1bb23077ddaf6184531c934e1b3a
Adekabang <iban.ninja@gmail.com>
2023-04-27 20:47:13 +0700

    Initial Commit

---
+++ NewsReaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReaderApp
+//
+//  Created by Raska Mohammad on 27/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
