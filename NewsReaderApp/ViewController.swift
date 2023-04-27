2fbb6b27f33b6f8cf47fb671dd469569f90d06ae
Adekabang <iban.ninja@gmail.com>
2023-04-27 22:47:23 +0700

    day 1 - create tabview and login layout

--- NewsReaderApp/ViewController.swift
+++ NewsReaderApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailTitleLabel: UILabel!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordTitleLabel: UILabel!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        emailTitleLabel.text = "Surel"
+        passwordTitleLabel.text = "Kata Sandi"
+        loginButton.setTitle("Masuk", for: UIControl.State.normal)
+        loginButton.layer.cornerRadius = 8
     }
 
-
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("Email:  \(emailTextField.text ?? "-")")
+    }
+    
 }
 
